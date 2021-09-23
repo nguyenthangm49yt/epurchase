2fa1e54acb7fb14e0ad8bd360067dc8e29112fa8
nguyenthangm49yt <nguyenthangm49yt@gmail.com>
2021-09-23 14:11:02 +0700

    test

---
+++ src/Component/Type.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Type extends Component {
+    render() {
+        return (
+            <div>
+                <p>text</p>
+            </div>
+        );
+    }
+}
+
+export default Type;
